b1c72ba6c2fff65805f82a0115b2d6000ab4e84a
philipqueen <queen.philip@gmail.com>
2024-12-31 15:37:03 -0500

    working pipeline (but bad results!)

--- intrinsics.py
+++ intrinsics.py
@@ -5,15 +5,16 @@ from skellytracker.trackers.charuco_tracker.charuco_tracker import CharucoTracke
 
 # flag definitions -> https://docs.opencv.org/4.x/d9/d0c/group__calib3d.html#ga3207604e4b1a1758aa66acb6ed5aa65d
 flags = (
-            # cv2.CALIB_USE_INTRINSIC_GUESS +  #starting values for camera matrix are provided
-            # cv2.CALIB_ZERO_TANGENT_DIST +  # p1 and p2 are set to zero and not changed
-            cv2.CALIB_FIX_ASPECT_RATIO + # keeps fx/fy ratio constant 
-            cv2.CALIB_FIX_PRINCIPAL_POINT +  # doesn't change center point
-            # cv2.CALIB_FIX_FOCAL_LENGTH
-            cv2.CALIB_RATIONAL_MODEL  # solves for k4, k5, k6 (adds additional points to distortion coefficients)
-            # cv2.CALIB_THIN_PRISM_MODEL
-            # cv2.CALIB_TILTED_MODEL
-            ) # TODO: this doesn't need to be a global, just setting it here for now
+    # cv2.CALIB_USE_INTRINSIC_GUESS +  #starting values for camera matrix are provided
+    # cv2.CALIB_ZERO_TANGENT_DIST +  # p1 and p2 are set to zero and not changed
+    cv2.CALIB_FIX_ASPECT_RATIO + # keeps fx/fy ratio constant
+    cv2.CALIB_FIX_PRINCIPAL_POINT + # doesn't change center point
+    # cv2.CALIB_FIX_FOCAL_LENGTH
+    cv2.CALIB_RATIONAL_MODEL  # solves for k4, k5, k6 (adds additional points to distortion coefficients)
+    # cv2.CALIB_THIN_PRISM_MODEL
+    # cv2.CALIB_TILTED_MODEL
+)  # TODO: this doesn't need to be a global, just setting it here for now
+
 
 def setup_5x7_tracker() -> CharucoTracker:
     charuco_squares_x_in = 7
@@ -28,6 +29,47 @@ def setup_5x7_tracker() -> CharucoTracker:
         dictionary=cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250),
     )
 
+
+def save_corrected_video(
+    input_video_path: Path,
+    output_video_path: Path,
+    camera_matrix: np.ndarray,
+    dist_coeffs: np.ndarray,
+):
+    print("Saving corrected video...")
+    cap = cv2.VideoCapture(str(input_video_path))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+    new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, dist_coeffs, (width, height), 1, (width, height))
+    x, y, w, h = roi
+
+    out = cv2.VideoWriter(
+        str(output_video_path),
+        cv2.VideoWriter.fourcc(*"mp4v"),
+        cap.get(cv2.CAP_PROP_FPS),
+        (w, h),
+    )
+
+    while True:
+        ret, image = cap.read()
+
+        if not ret:
+            break
+
+        corrected_image = cv2.undistort(image, camera_matrix, dist_coeffs, None, new_camera_matrix)
+        cropped_corrected_image = corrected_image[y:y+h, x:x+w]
+
+        cv2.imshow("Corrected Image", cropped_corrected_image)
+        cv2.waitKey(1)
+        out.write(cropped_corrected_image)
+
+    cap.release()
+    out.release()
+
+    print(f"Corrected video saved to {output_video_path}")
+
+
 def run_intrinsics(input_video_path: Path, output_video_path: Path):
     charuco_tracker = setup_5x7_tracker()
 
@@ -40,16 +82,20 @@ def run_intrinsics(input_video_path: Path, output_video_path: Path):
     all_image_points = []
     all_object_points = []
 
-    # all_images = []
-
     cap = cv2.VideoCapture(str(input_video_path))
 
-    image_size = (int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
-    camera_matrix = np.array([[float(image_size[1]), 0, image_size[1] / 2], [0, float(image_size[0]), image_size[0] / 2], [0, 0, 1]])
+    image_size = (
+        int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
+        int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+    )
+    camera_matrix = np.array(
+        [
+            [float(image_size[1]), 0, image_size[1] / 2],
+            [0, float(image_size[0]), image_size[0] / 2],
+            [0, 0, 1],
+        ]
+    )
     dist_coeffs = np.zeros((5, 1))
-
-    all_camera_matrices = []
-    all_dist_coeffs = []
 
     frame_number = -1
     while True:
@@ -64,35 +110,65 @@ def run_intrinsics(input_video_path: Path, output_video_path: Path):
         current_object_points = np.array([])
         current_image_points = np.array([])
 
-        current_charuco_corners, current_charuco_ids, _, _ = (
-            detector.detectBoard(gray_image)
+        current_charuco_corners, current_charuco_ids, _, _ = detector.detectBoard(
+            gray_image
         )
 
-        if current_charuco_corners is not None and len(current_charuco_corners) > 5:
-            current_object_points, current_image_points = board.matchImagePoints(current_charuco_corners, current_charuco_ids, current_object_points, current_image_points)
+        if (
+            current_charuco_corners is not None and len(current_charuco_corners) >= 8
+        ):  # Documentaiton says >= 6, but osmetimes throws an error with frames with fewer corners
+            current_object_points, current_image_points = board.matchImagePoints(
+                current_charuco_corners,
+                current_charuco_ids,
+                current_object_points,
+                current_image_points,
+            )
 
             if not current_image_points.any() or not current_object_points.any():
                 print(f"Point matching failed for frame {frame_number}.")
-                continue
+            else:
+                print(f"Frame captured: {frame_number}")
 
-            print(f"Frame captured: {frame_number}")
+                all_charuco_corners.append(current_charuco_corners)
+                all_charuco_ids.append(current_charuco_ids)
+                all_image_points.append(current_image_points)
+                all_object_points.append(current_object_points)
 
-            all_charuco_corners.append(current_charuco_corners)
-            all_charuco_ids.append(current_charuco_ids)
-            all_image_points.append(current_image_points)
-            all_object_points.append(current_object_points)
-            # all_images.append(image)
+    print("Debugging Info")
+    print(f"\tlength of all_charuco_corners: {len(all_charuco_corners)}")
+    print(f"\tlength of all_charuco_ids: {len(all_charuco_ids)}")
+    print(f"\tlength of all_image_points: {len(all_image_points)}")
+    print(f"\tlength of all_object_points: {len(all_object_points)}")
 
     ret, camera_matrix, dist_coeffs, _, _ = cv2.calibrateCamera(
-        all_object_points, all_image_points, image_size, camera_matrix, dist_coeffs, flags=flags
-    ) # TODO: only call this at the end, or every X landmarks
+        all_object_points,
+        all_image_points,
+        image_size,
+        camera_matrix,
+        dist_coeffs,
+        flags=flags,
+    )
 
     print(f"Camera matrix: {camera_matrix}")
     print(f"Distortion coefficients: {dist_coeffs}")
 
     cap.release()
 
+    save_corrected_video(
+        input_video_path=input_video_path,
+        output_video_path=output_video_path,
+        camera_matrix=camera_matrix,
+        dist_coeffs=dist_coeffs,
+    )
+
 
 if __name__ == "__main__":
-    input_video_path = Path("/Users/philipqueen/freemocap_data/recording_sessions/session_2024-06-27_15_07_36/recording_15_13_46_gmt-4_calibration/synchronized_videos/Camera_000_synchronized.mp4")
-    run_intrinsics(input_video_path=input_video_path, output_video_path=None)
+    input_video_path = Path(
+        "/Users/philipqueen/freemocap_data/recording_sessions/session_2024-06-27_15_07_36/recording_15_13_46_gmt-4_calibration/synchronized_videos/Camera_000_synchronized.mp4"
+    )
+    output_video_path = input_video_path.with_name(
+        f"{input_video_path.stem}_corrected.mp4"
+    )
+    run_intrinsics(
+        input_video_path=input_video_path, output_video_path=output_video_path
+    )
