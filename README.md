42a5a980f017c9e6879e1d8fea81f55e227bceae
philipqueen <queen.philip@gmail.com>
2025-01-03 12:53:21 -0700

    intrinsics working well!

--- README.md
+++ README.md
@@ -3,10 +3,12 @@
 ## Intrinsics
 Find Jon's example at: https://github.com/freemocap/freemocap/blob/jon/src-necromancy/src/core_processor/camera_calibration/lens_distortion_calibrator.py
 
-
+Need to find some way to filter for only good/different frames - we don't need as many views as we're getting, but we need good views with as different poses as possible
 
 
 ### Resources
 OpenCV Docs:
 Calibration flags: https://docs.opencv.org/4.x/d9/d0c/group__calib3d.html#ga3207604e4b1a1758aa66acb6ed5aa65d
+Intrinsics in Python: https://docs.opencv.org/4.x/dc/dbb/tutorial_py_calibration.html
+
 Calibration Best Practices - Calib.io: https://calib.io/blogs/knowledge-base/calibration-best-practices
--- intrinsics.py
+++ intrinsics.py
@@ -3,17 +3,20 @@ import cv2
 import numpy as np
 from skellytracker.trackers.charuco_tracker.charuco_tracker import CharucoTracker
 
+np.set_printoptions(suppress=True)
+
 # flag definitions -> https://docs.opencv.org/4.x/d9/d0c/group__calib3d.html#ga3207604e4b1a1758aa66acb6ed5aa65d
 flags = (
     # cv2.CALIB_USE_INTRINSIC_GUESS +  #starting values for camera matrix are provided
     # cv2.CALIB_ZERO_TANGENT_DIST +  # p1 and p2 are set to zero and not changed
     cv2.CALIB_FIX_ASPECT_RATIO + # keeps fx/fy ratio constant
-    cv2.CALIB_FIX_PRINCIPAL_POINT + # doesn't change center point
+    cv2.CALIB_FIX_PRINCIPAL_POINT #+ # doesn't change center point
     # cv2.CALIB_FIX_FOCAL_LENGTH
-    cv2.CALIB_RATIONAL_MODEL  # solves for k4, k5, k6 (adds additional points to distortion coefficients)
+    # cv2.CALIB_RATIONAL_MODEL  # solves for k4, k5, k6 (adds additional points to distortion coefficients)
     # cv2.CALIB_THIN_PRISM_MODEL
     # cv2.CALIB_TILTED_MODEL
 )  # TODO: this doesn't need to be a global, just setting it here for now
+# TODO: I've mostly narrowed down to flags we probably don't need. We may be able to skip this part.
 
 
 def setup_5x7_tracker() -> CharucoTracker:
@@ -41,7 +44,10 @@ def save_corrected_video(
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 
-    new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, dist_coeffs, (width, height), 1, (width, height))
+    include_all_pixels = 1 # 1: all pixels are retained with some extra black in margins, 0: only valid pixels are shown
+    # we can do 1 here because we crop it down anyways
+
+    new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, dist_coeffs, (width, height), include_all_pixels, (width, height))
     x, y, w, h = roi
 
     out = cv2.VideoWriter(
@@ -85,17 +91,9 @@ def run_intrinsics(input_video_path: Path, output_video_path: Path):
     cap = cv2.VideoCapture(str(input_video_path))
 
     image_size = (
-        int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
         int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
     )
-    camera_matrix = np.array(
-        [
-            [float(image_size[1]), 0, image_size[1] / 2],
-            [0, float(image_size[0]), image_size[0] / 2],
-            [0, 0, 1],
-        ]
-    )
-    dist_coeffs = np.zeros((5, 1))
 
     frame_number = -1
     while True:
@@ -140,12 +138,14 @@ def run_intrinsics(input_video_path: Path, output_video_path: Path):
     print(f"\tlength of all_image_points: {len(all_image_points)}")
     print(f"\tlength of all_object_points: {len(all_object_points)}")
 
+    # TODO: filter for "better" views of the charuco somehow, and possibly filter for "most different" views to get variety
+
     ret, camera_matrix, dist_coeffs, _, _ = cv2.calibrateCamera(
         all_object_points,
         all_image_points,
         image_size,
-        camera_matrix,
-        dist_coeffs,
+        None,
+        None,
         flags=flags,
     )
 
@@ -163,9 +163,10 @@ def run_intrinsics(input_video_path: Path, output_video_path: Path):
 
 
 if __name__ == "__main__":
-    input_video_path = Path(
-        "/Users/philipqueen/freemocap_data/recording_sessions/session_2024-06-27_15_07_36/recording_15_13_46_gmt-4_calibration/synchronized_videos/Camera_000_synchronized.mp4"
-    )
+    # input_video_path = Path(
+    #     "/Users/philipqueen/freemocap_data/recording_sessions/session_2024-06-27_15_07_36/recording_15_13_46_gmt-4_calibration/synchronized_videos/Camera_000_synchronized.mp4"
+    # )
+    input_video_path = Path("/Users/philipqueen/Movies/intrinsic_calibration_laptopcam.mp4")
     output_video_path = input_video_path.with_name(
         f"{input_video_path.stem}_corrected.mp4"
     )
